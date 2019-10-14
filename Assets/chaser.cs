d5a3e17a897caa0bc3d90109506f00bc98507de7
natedonato <natedonato@gmail.com>
2019-10-14 11:54:21 -0700

    chaser setup

---
+++ Assets/chaser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chaser : MonoBehaviour
+{
+    public Transform targetTransform; // drag the player cube into this field in unity editor to get the player transform data
+    public float moveSpeed = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 displacementFromTarget = targetTransform.position - transform.position;
+        Vector3 directionToTarget = displacementFromTarget.normalized;
+        Vector3 velocity = directionToTarget * moveSpeed;
+
+        float distanceToTarget = displacementFromTarget.magnitude;
+        if (distanceToTarget > 1.5)
+        {
+            transform.Translate(velocity * Time.deltaTime);
+        }
+    }
+}
